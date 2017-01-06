101cb2e65fb752e5b2686f98c4f9e64a3a8397ac
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2017-01-06 04:25:33 +0100

    Read serial no of temp sensor

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # esp32_weather
 Collect data from bosh/sparkfun BME280 i2c and internal temperature sensor. Send data to thingspeak.com and go back to sleep.
 
+First test I try reading the serial number from the si720 temperature humidity board.
+
+https://cdn-learn.adafruit.com/assets/assets/000/035/931/original/Support_Documents_TechnicalDocs_Si7021-A20.pdf
+
+#The si720 board
+https://cdn-learn.adafruit.com/downloads/pdf/adafruit-si7021-temperature-plus-humidity-sensor.pdf
+
 
 #The weather data board
 https://github.com/sparkfun/SparkFun_BME280_Arduino_Library
--- main/app_main.c
+++ main/app_main.c
@@ -15,6 +15,145 @@
 
 #include "driver/gpio.h"
 #include "sdkconfig.h"
+#include "driver/i2c.h"
+
+// Use same inputs as sparkfun
+
+#define I2C_MASTER_SCL_IO    22    /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO    21    /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM   I2C_NUM_0   /*!< I2C port number for master dev */
+#define I2C_MASTER_TX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
+#define I2C_MASTER_FREQ_HZ    100000     /*!< I2C master clock frequency */
+
+#define WRITE_BIT  I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT   I2C_MASTER_READ  /*!< I2C master read */
+#define ACK_CHECK_EN   0x1     /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS  0x0     /*!< I2C master will not check ack from slave */
+#define ACK_VAL    0x0         /*!< I2C ack value */
+#define NACK_VAL   0x1         /*!< I2C nack value */
+
+
+/**
+ * @brief test function to show buffer
+ */
+void show_buf(uint8_t* buf, int len)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        printf("%02x ", buf[i]);
+        if (( i + 1 ) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+#define  SI7021_ADDRESS    0x40
+#define  SN_CMD_START      0xFA
+#define  SN_CMD2           0x0F
+
+/**
+ *    https://cdn-learn.adafruit.com/assets/assets/000/035/931/original/Support_Documents_TechnicalDocs_Si7021-A20.pdf
+ * @brief test code to read serial number from Si7021 device
+ * _________________________________________________________________
+ * | start | slave_addr + wr_bit + ack | write 1 byte + ack  | stop |
+ * --------|---------------------------|---------------------|------|
+ * 2. wait more than 24 ms
+ * 3. read data
+ * ______________________________________________________________________________________
+ * | start | slave_addr + rd_bit + ack | read 1 byte + ack  ... read 1 byte + nack | stop |
+ * --------|---------------------------|---------------------|--------------------|------|
+ */
+esp_err_t i2c_sn7021_test(i2c_port_t i2c_num, uint8_t* sna_3,uint8_t* sna_2,uint8_t* sna_1,uint8_t* sna_0)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, SI7021_ADDRESS << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, SN_CMD_START, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, SN_CMD2, ACK_CHECK_EN);
+
+    i2c_master_stop(cmd);
+    int ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    if (ret == ESP_FAIL) {
+        return ret;
+    }
+    vTaskDelay(30 / portTICK_RATE_MS);
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, SI7021_ADDRESS << 1 | READ_BIT, ACK_CHECK_EN);
+    i2c_master_read_byte(cmd, sna_3, ACK_VAL);
+    i2c_master_read_byte(cmd, sna_2, ACK_VAL);
+    i2c_master_read_byte(cmd, sna_1, ACK_VAL);
+    i2c_master_read_byte(cmd, sna_0, NACK_VAL);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    if (ret == ESP_FAIL) {
+        return ESP_FAIL;
+    }
+    return ESP_OK;
+}
+
+
+
+/**
+ *
+ * _______________________________________________________________________________________
+ * | start | slave_addr  +ack | 
+ * --------|--------------------------|----------------------|--------------------|------|
+ *
+ */
+esp_err_t i2c_master_check_slave(i2c_port_t i2c_num,uint8_t addr)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ( addr << 1 ) , ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+
+/**
+ * @brief i2c master initialization
+ */
+void i2c_init()
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+
+
+/**
+ * @brief Scan all i2c adresses 
+ */
+void i2c_scan() {
+	int address;
+    int ret;
+	int foundCount = 0;
+	for (address=1; address<127; address++) {
+        ret=i2c_master_check_slave(I2C_MASTER_NUM,address);
+        if (ret == ESP_OK) {
+            printf("Found device addres: %02x\n", address);
+        }
+    }
+    printf("Done scanning\n");
+
+}
+
 
 #define BLINK_GPIO 5
 
@@ -40,11 +179,24 @@ void blink_task(void *pvParameters)
 
 void app_main()
 {
-
+    int ret;
 	printf("Starting ... \r\n");
 
-	xTaskCreatePinnedToCore(&blink_task, "blink_task", 1024, NULL, 5,
-				NULL, 0);
 
+    uint8_t sna3,sna2,sna1,sna0; 
+    i2c_init();
+
+    ret = i2c_sn7021_test( I2C_MASTER_NUM, &sna3, &sna2,&sna1,&sna0);
+
+    if (ret == ESP_OK) {
+        printf("s/n: %02x %02x %02x %02x\n", sna3,sna2,sna1,sna0);
+    } else {
+        printf("No ack, sensor not connected...skip...\n");
+    }
+
+    i2c_scan();
+
+    //xTaskCreatePinnedToCore(&blink_task, "blink_task", 1024, NULL, 5,
+	//			NULL, 0);
 
 }
